55442cf7668dddc56d189ad0a665583db4dab913
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-05-03 00:57:35 +0100

    add express middleware static

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(cors());
+app.use(express.static('build')) 
 
 // define a custom token for morgan
 morgan.token("req-body", (req, res) => {
