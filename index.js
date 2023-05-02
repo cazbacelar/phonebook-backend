fc01ce9897dc809c7c8c167c1a40907761ec2077
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-05-02 14:31:23 +0100

    add /info route to the app

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 
 let persons = [
   {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
   {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
 ]
 
 // application's / root
-app.get('/', (request, response) => {
-  response.send('<h1>Root</h1>')
+app.get("/", (request, response) => {
+  response.send("<h1>Root</h1>")
 })
 
-app.get('/api/persons', (request, response) => {
+// returns all persons
+app.get("/api/persons", (request, response) => {
   response.json(persons)
+})
+
+// returns info
+app.get("/info", (request, response) => {
+  const length = persons.length
+  const currentTime = new Date()
+
+  response.send(`<p>Phonebook has info for ${length} people.</p><p>Request received at ${currentTime}</p>`)
 })
 
 const PORT = 3001
